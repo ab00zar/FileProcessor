538dd9167dc42846f6d4e2318f1b99f474d3065b
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-04-07 10:53:23 +0200

    some changes in model, form, show, index file to upload files and show their download link

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,4 +2,6 @@ class Treatment < ApplicationRecord
   has_attached_file :file1
   has_attached_file :file2
   has_attached_file :result
+  validates :file1, :file2, attachment_presence: true
+  do_not_validate_attachment_file_type :file1, :file2
 end
