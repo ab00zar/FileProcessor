92bca22e147702ccccad8c90eb878d15123e3406
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-04-08 16:52:42 +0200

    config the root for users and non-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :treatments
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root "treatments#index", as: "authenticated_root"
+  end
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
